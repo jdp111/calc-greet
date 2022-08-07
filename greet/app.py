325c752c5f0035878479356118382569edb5e64e
jdp111 <jdpearce111@gmail.com>
2022-08-07 14:01:05 -0600

    completed greeter

---
+++ greet/app.py
@@ -0,0 +1,11 @@
+# Put your app in here.
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/welcome")
+def welcome_empty():
+    return f"welcome"
+
+@app.route("/welcome/<place>")
+def welcome_statement(place):
+    return f"welcome {place}"
